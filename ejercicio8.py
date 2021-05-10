a707fdc304b06660b19ae5964d3f123ddcdda559
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-09 22:05:03 -0500

    Add files via upload

---
+++ ejercicio8.py
@@ -0,0 +1,3 @@
+lado  =  float ( input ( "Ingrese la longitud de un lado del hexágono:" ))
+apotema  =  float ( input ( "Ingrese la longitud de la apotema del hexágono:" ))
+print ( "El área del hexágono es:" , (( lado  *  6 ) *  apotema ) /  2 )
